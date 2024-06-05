7af2400dcab5120cab58c41ea339439d3a7602ee
Neo <neotheprogramist@proton.me>
2024-06-05 14:49:51 +0200

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ use clap::Parser;
 use rand::{prelude::SliceRandom, seq::IteratorRandom};
 use serde::{Deserialize, Serialize};
 
+const DIGITS: &str = "123456789";
+const SPECIAL: &str = "!@#$%^&*";
+const ALL: &str = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789!@#$%^&*";
+
 #[derive(Parser, Debug, Serialize, Deserialize)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
@@ -16,25 +20,22 @@ pub struct Args {
 }
 
 /// Generates a password based on specified constraints for length, number of digits, and number of special characters.
+#[must_use]
 pub fn generate_password(length: u32, digits: u32, specials: u32) -> String {
     let mut rng = rand::thread_rng();
-
-    const DIGITS: &str = "123456789";
-    const SPECIAL: &str = "!@#$%^&*";
-    const ALL: &str = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789!@#$%^&*";
 
     let mut result = Vec::new();
 
     for _ in 0..digits {
-        result.push(DIGITS.chars().choose(&mut rng).unwrap());
+        result.push(DIGITS.chars().choose(&mut rng).unwrap_or_default());
     }
 
     for _ in 0..specials {
-        result.push(SPECIAL.chars().choose(&mut rng).unwrap());
+        result.push(SPECIAL.chars().choose(&mut rng).unwrap_or_default());
     }
 
     for _ in 0..(length - digits - specials) {
-        result.push(ALL.chars().choose(&mut rng).unwrap());
+        result.push(ALL.chars().choose(&mut rng).unwrap_or_default());
     }
 
     result.shuffle(&mut rng);
